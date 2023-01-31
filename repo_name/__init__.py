be00d33507359a067f63e85c35deead068814c9e
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-01-31 22:17:41 +0100

    Change 2022 to 2023

--- repo_name/__init__.py
+++ repo_name/__init__.py
@@ -2,7 +2,7 @@ from repo_name.example import hello
 
 
 __version__ = "0.0.1"
-__copyright__ = "Copyright (C) 2022 Shuhei Watanabe"
+__copyright__ = "Copyright (C) 2023 Shuhei Watanabe"
 __licence__ = "Apache-2.0 License"
 __author__ = "Shuhei Watanabe"
 __author_email__ = "shuhei.watanabe.utokyo@gmail.com"
